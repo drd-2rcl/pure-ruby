d92966a3e6eb1fab5c47b6f88e4af02396b48553
drd-2rcl <drd.rclrd@gmail.com>
2020-05-31 18:57:21 -0300

    Add example to loop in hash

---
+++ 02.colletions/02.each_hash.rb
@@ -0,0 +1,11 @@
+aulas = {
+  'Aula 1' => 'liberada', 
+  'Aula 2' => 'liberada', 
+  'Aula 3' => 'liberada', 
+  'Aula 4' => 'liberada', 
+  'Aula 5' => 'liberada'
+}
+
+aulas.each do |key, value|
+  puts "#{key} #{value}"
+end
