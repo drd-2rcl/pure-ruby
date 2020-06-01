edde26b57467000cae6c0477c83baa45ea397425
drd-2rcl <drd.rclrd@gmail.com>
2020-06-01 18:20:42 -0300

    Example about instance variable

---
+++ 05.POO-II/07.intance_var.rb
@@ -0,0 +1,16 @@
+class User
+  
+  def add(name)
+    @name = name
+    puts "User adicionado"
+    hello
+  end
+
+  def hello
+    puts "Seja bem vindo, #{@name}"
+  end
+
+end
+
+user = User.new
+user.add('Joao')
