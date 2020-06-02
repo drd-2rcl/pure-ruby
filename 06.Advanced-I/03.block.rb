d0f1a9cefb4fc23bca5b96507cbc289e7691455c
drd-2rcl <drd.rclrd@gmail.com>
2020-06-02 09:48:49 -0300

    In blocks that use multiple lines, make use of the do ... end

---
+++ 06.Advanced-I/03.block.rb
@@ -0,0 +1,9 @@
+#  in blocks that use multiple lines, make use of the do ... end
+foo = {2 => 3, 4 => 5}
+
+foo.each do |key, value|
+  puts "key = #{key}"
+  puts "value = #{value}"
+  puts "key * value = #{key * value}"
+  puts "---"
+end
