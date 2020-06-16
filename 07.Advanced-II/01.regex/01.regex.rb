f67e5be730d20c0b1bf4061a5244ec88e791802d
drd-2rcl <drd.rclrd@gmail.com>
2020-06-15 21:10:28 -0300

    Example about regex

---
+++ 07.Advanced-II/01.regex/01.regex.rb
@@ -0,0 +1,16 @@
+# Exemplos
+
+# 2.7.0 :001 > /abcd/
+#  => /abcd/
+# 2.7.0 :002 > /abcd/.class
+#  => Regexp
+# 2.7.0 :003 > %r{abcd}
+#  => /abcd/
+# 2.7.0 :004 > %r{abcd}.class
+#  => Regexp
+# 2.7.0 :005 > Regexp.new('expressao')
+#  => /expressao/
+# 2.7.0 :006 > Regexp.new('expressao').class
+#  => Regexp
+# 2.7.0 :007 >
+
